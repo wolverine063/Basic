40893c0ed529d36e957e5f1fd5c23b94d720fb1d
wolverine063 <netflixwol89@gmail.com>
2025-08-13 21:25:32 -0700

    Add backend with MongoDB connection and secure auth2

--- backend/server.js
+++ backend/server.js
@@ -45,4 +45,5 @@ app.post("/api/signup", async (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+
